9e9179a6839ed8325a8acd051b504e568fb77a16
Dearian <dearian2706@gmail.com>
2022-09-19 20:59:26 +0300

    frame

---
+++ src/components/Info/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './Info.module.scss'
+
+const Info = () => {
+  return(
+    <aside className={styles.info}>
+
+    </aside>
+  )
+}
+
+export default Info
